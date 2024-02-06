0cb44dd9a6e04286c77edbb1943a4e1a688d42d9
Lucia <Luciaabouhamad@gmail.com>
2024-02-06 09:34:59 +0100

    testing pipline if test fails(should not poblish)

--- TestdrivenHotel.Test/UnitTest1.cs
+++ TestdrivenHotel.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace TestdrivenHotel.Test
 {
     public class UnitTest1
@@ -5,6 +7,11 @@ namespace TestdrivenHotel.Test
         [Fact]
         public void Test1()
         {
+            string expected = "Hello world";
+
+            string result = "ello world";
+
+            result.Should().BeEquivalentTo(expected);
 
         }
     }
