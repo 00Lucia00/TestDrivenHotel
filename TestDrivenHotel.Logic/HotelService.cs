a4373efef1a62a3c4829401b410ba862d333f61b
Lucia <Luciaabouhamad@gmail.com>
2024-02-08 14:32:26 +0100

    secound test cleard (if unvalied date choies list should be empty)

--- TestDrivenHotel.Logic/HotelService.cs
+++ TestDrivenHotel.Logic/HotelService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,49 @@ using TestDrivenHotel.DataAccess;
 
 namespace TestDrivenHotel.Logic
 {   //Denna klass ska innehålla "CRUD"(typ) funktionalitet.
-    public class HotelService
+    public class HotelService 
     {
 
 
         //Create - Skapa
 
         //Read - Läsa
+
+        // Denna funktion hämtar tillgänliga rum utifrån sökkriteria
         public List<RoomModel> GetAvailableRooms(DateTime checkInDate, DateTime checkOutDate, int numberOfGuests, string roomType = null)
         {
+            // lägger till det rum som har kapasietet för det anatlet gäster man söker i separat variabel
             var availableRooms = HotelData.Rooms.Where(r => r.GuestCapacity >= numberOfGuests).ToList();
 
-            return availableRooms;
+            // kollar om man valt till rumtyp - annars visas alla rumtyper utifrån det andra kriterierna
+            if (!string.IsNullOrEmpty(roomType))
+            {
+                availableRooms = availableRooms.Where(r => r.RoomType == roomType).ToList();
+            }
+
+            if (checkInDate < DateTime.Now || checkOutDate < checkInDate)
+            {
+                availableRooms.Clear();
+                return availableRooms;
+            }
+            else 
+            {
+                // kollar ifall datumen man sökt efter är tillgängliga och inte ligger i bookings listan
+                foreach (var booking in HotelData.Bookings)
+                {
+                    if (checkInDate >= booking.CheckInDate && checkInDate < booking.CheckOutDate ||
+                        checkOutDate > booking.CheckInDate && checkOutDate <= booking.CheckOutDate ||
+                        checkInDate < booking.CheckInDate && checkOutDate > booking.CheckOutDate)
+                    {
+                        //tarbort det rum som är bokade under det sökta datumen
+                        availableRooms.Remove(availableRooms.First(r => r.Id == booking.RoomId));
+                    }
+                }
+                return availableRooms;
+            }
         }
+       
+
         //Update - Uppdatera
         //Delete - ta Bort
     }
--- TestdrivenHotel.Test/HotelServiceTest.cs
+++ TestdrivenHotel.Test/HotelServiceTest.cs
@@ -7,14 +7,18 @@ namespace TestdrivenHotel.Test
 {
     public class HotelServiceTest
     {
+        private readonly HotelService service;
 
-
+        public HotelServiceTest()
+        {
+            service = new HotelService();
+        }
 
         [Fact]
         public void GetAvailableRooms_WhenNoBookings_ShouldReturnExpectedRooms()
         {
             // Given - Arrange
-            HotelService service = new HotelService();
+            
             var checkInDate = DateTime.Now;
             var checkOutDate = checkInDate.AddDays(3);
             var numberOfGuests = 2;
@@ -25,6 +29,23 @@ namespace TestdrivenHotel.Test
             availableRooms.Should().NotBeEmpty();
             availableRooms.Should().OnlyContain(r => r.GuestCapacity >= numberOfGuests);
 
+        }
+
+        // test checking if list is empty when dates not avalible
+        [Fact]
+        public void GetAvailableRooms_ReturnsEmptyList_WhenCalledWithCheckOutDateBeforeCheckInDate()
+        {
+            // Arrange
+           
+            var checkInDate = DateTime.Now;
+            var checkOutDate = checkInDate.AddDays(-2);
+            var numberOfGuests = 1;
+
+            // Act
+            var availableRooms = service.GetAvailableRooms(checkInDate, checkOutDate, numberOfGuests);
+
+            // Assert
+            availableRooms.Should().BeEmpty();
         }
     }
 }
